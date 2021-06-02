13edac19e6f253aef694c199a71491410e241488
blithe <1528663970@qq.com>
2021-06-02 23:44:19 +0800

    地区销量排行完成

---
+++ vision/src/components/Rank.vue
@@ -0,0 +1,150 @@
+<template>
+  <div class="com-container">
+    <div class="com-chart" ref="rank_ref"></div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      chartInstance: null,
+      allData: null,
+      startValue: 0,
+      endValue: 9,
+      timerId: null,
+    };
+  },
+  mounted() {
+    this.initchart();
+    this.getData();
+    window.addEventListener("resize", this.screenAdapter);
+    this.screenAdapter();
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.screenAdapter);
+    clearInterval(this.timerId);
+  },
+  methods: {
+    initchart() {
+      this.chartInstance = this.$echarts.init(this.$refs.rank_ref, "chalk");
+      const initOption = {
+        title: {
+          text: "┃ 地区销售排行",
+          left: 20,
+          top: 20,
+        },
+        grid: {
+          top: "40%",
+          left: "5%",
+          right: "5%",
+          bottom: "5%",
+          containLabel: true,
+        },
+        tooltip: {
+          show: true,
+        },
+        xAxis: {
+          type: "category",
+        },
+        yAxis: {
+          type: "value",
+        },
+        series: [{ type: "bar" }],
+      };
+      this.chartInstance.setOption(initOption);
+      this.chartInstance.on("mouseover", () => {
+        clearInterval(this.timerId);
+      });
+      this.chartInstance.on("mouseout", () => {
+        this.startInterval();
+      });
+    },
+    async getData() {
+      const { data: ret } = await this.$http.get("rank");
+      this.allData = ret;
+      this.allData.sort((a, b) => b.value - a.value);
+      console.log(this.allData);
+      this.updateChart();
+      this.startInterval();
+    },
+    updateChart() {
+      const colorArr = [
+        ["#0BA82C", "#4FF778"],
+        ["#2E72BF", "#23E5E5"],
+        ["#5052EE", "#AB6EE5"],
+      ];
+      const provinceArr = this.allData.map((item) => item.name);
+      const valueArr = this.allData.map((item) => item.value);
+      const dataOption = {
+        xAxis: {
+          data: provinceArr,
+        },
+        dataZoom: {
+          show: true,
+          startValue: this.startValue,
+          endValue: this.endValue,
+        },
+        series: [
+          {
+            data: valueArr,
+            itemStyle: {
+              color: (arg) => {
+                let targetColorArr = null;
+                if (arg.value > 300) {
+                  targetColorArr = colorArr[0];
+                } else if (arg.value > 200) {
+                  targetColorArr = colorArr[1];
+                } else {
+                  targetColorArr = colorArr[2];
+                }
+                return new this.$echarts.graphic.LinearGradient(0, 0, 0, 1, [
+                  { offset: 0, color: targetColorArr[0] },
+                  { offset: 1, color: targetColorArr[1] },
+                ]);
+              },
+            },
+          },
+        ],
+      };
+      this.chartInstance.setOption(dataOption);
+    },
+    screenAdapter() {
+      const titleFontSize = (this.$refs.rank_ref.offsetWidth / 100) * 3.6;
+      const adapterOption = {
+        title: {
+          textStyle: {
+            fontSize: titleFontSize,
+          },
+        },
+        series: [
+          {
+            barWidth: titleFontSize,
+            itemStyle: {
+              barBorderRadius: [titleFontSize / 2, titleFontSize / 2, 0, 0],
+            },
+          },
+        ],
+      };
+      this.chartInstance.setOption(adapterOption);
+    },
+    startInterval() {
+      if (this.timerId) {
+        clearInterval(this.timerId);
+      }
+      this.timerId = setInterval(() => {
+        this.startValue++;
+        this.endValue++;
+        this.updateChart();
+        if (this.endValue > this.allData.length - 1) {
+          this.startValue = 0;
+          this.endValue = 9;
+        }
+      }, 2000);
+    },
+  },
+};
+</script>
+
+<style lang="less" scoped>
+</style>
